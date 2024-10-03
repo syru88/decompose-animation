585f3f72a6ba21a5e8d4e38fdb041bafe59aafa1
Marcel Syrucek <marcel.syrucek@gmail.com>
2024-10-03 18:05:41 +0200

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,16 @@
+@Suppress("DSL_SCOPE_VIOLATION")
+
+plugins {
+    //trick: for the same plugin versions in all sub-modules
+    alias(libs.plugins.android.application).apply(false)
+    alias(libs.plugins.android.library).apply(false)
+    alias(libs.plugins.kotlin.android).apply(false)
+    alias(libs.plugins.kotlin.compose).apply(false)
+    alias(libs.plugins.kotlin.multiplatform).apply(false)
+    alias(libs.plugins.kotlin.serialization).apply(false)
+    alias(libs.plugins.jetbrains.compose).apply(false)
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.layout.buildDirectory)
+}
