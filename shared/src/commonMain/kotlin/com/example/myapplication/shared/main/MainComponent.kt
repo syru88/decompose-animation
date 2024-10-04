ff2948cb9eb5e84bf2b7c7de36de32873e309fc4
Marcel Syrucek <marcel.syrucek@gmail.com>
2024-10-04 09:13:18 +0200

    Squash components

--- shared/src/commonMain/kotlin/com/example/myapplication/shared/main/MainComponent.kt
+++ shared/src/commonMain/kotlin/com/example/myapplication/shared/main/MainComponent.kt
@@ -1,8 +1,21 @@
 package com.example.myapplication.shared.main
 
+import com.arkivanov.decompose.ComponentContext
+
 interface MainComponent {
 
     val counter: Int
 
     fun onShowWelcomeClicked()
+}
+
+class DefaultMainComponent(
+    componentContext: ComponentContext,
+    override val counter: Int,
+    private val onShowWelcome: () -> Unit,
+) : MainComponent, ComponentContext by componentContext {
+
+    override fun onShowWelcomeClicked() {
+        onShowWelcome()
+    }
 }
--- shared/src/commonMain/kotlin/com/example/myapplication/shared/root/RootComponent.kt
+++ shared/src/commonMain/kotlin/com/example/myapplication/shared/root/RootComponent.kt
@@ -1,9 +1,18 @@
 package com.example.myapplication.shared.root
 
+import com.arkivanov.decompose.ComponentContext
 import com.arkivanov.decompose.router.stack.ChildStack
+import com.arkivanov.decompose.router.stack.StackNavigation
+import com.arkivanov.decompose.router.stack.childStack
+import com.arkivanov.decompose.router.stack.popTo
+import com.arkivanov.decompose.router.stack.replaceAll
 import com.arkivanov.decompose.value.Value
+import com.example.myapplication.shared.main.DefaultMainComponent
 import com.example.myapplication.shared.main.MainComponent
+import com.example.myapplication.shared.root.RootComponent.Child
+import com.example.myapplication.shared.welcome.DefaultWelcomeComponent
 import com.example.myapplication.shared.welcome.WelcomeComponent
+import kotlinx.serialization.Serializable
 
 interface RootComponent {
 
@@ -14,5 +23,58 @@ interface RootComponent {
     sealed class Child {
         class Main(val component: MainComponent) : Child()
         class Welcome(val component: WelcomeComponent) : Child()
+    }
+}
+
+class DefaultRootComponent(
+    componentContext: ComponentContext,
+) : RootComponent, ComponentContext by componentContext {
+
+    private val navigation = StackNavigation<Config>()
+
+    override val stack: Value<ChildStack<*, Child>> =
+        childStack(
+            source = navigation,
+            serializer = Config.serializer(),
+            initialConfiguration = Config.Main,
+            handleBackButton = true,
+            childFactory = ::child,
+        )
+
+    private var mainCounter = 0
+
+    private fun child(config: Config, childComponentContext: ComponentContext): Child =
+        when (config) {
+            is Config.Main -> {
+                Child.Main(mainComponent(childComponentContext))
+            }
+
+            is Config.Welcome -> Child.Welcome(welcomeComponent(childComponentContext))
+        }
+
+    private fun mainComponent(componentContext: ComponentContext): MainComponent =
+        DefaultMainComponent(
+            componentContext = componentContext,
+            counter = ++mainCounter,
+            onShowWelcome = { navigation.replaceAll(Config.Welcome) },
+        )
+
+    private fun welcomeComponent(componentContext: ComponentContext): WelcomeComponent =
+        DefaultWelcomeComponent(
+            componentContext = componentContext,
+            onFinished = { navigation.replaceAll(Config.Main) },
+        )
+
+    override fun onBackClicked(toIndex: Int) {
+        navigation.popTo(index = toIndex)
+    }
+
+    @Serializable
+    private sealed interface Config {
+        @Serializable
+        data object Main : Config
+
+        @Serializable
+        data object Welcome : Config
     }
 }
